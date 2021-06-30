101186b4d9f318416663d9959bff2f9200183907
HarshitaGupta16 <47924686+HarshitaGupta16@users.noreply.github.com>
2021-06-30 11:41:09 +0530

    created LikeMe.js

---
+++ Week-4/LikeMe.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [likeCounter, setLikeCounter] = useState(0);
+
+  function likeClickHandler() {
+    var newLikeCounter = likeCounter + 1;
+    setLikeCounter(newLikeCounter); // set the like counter value as the new value
+    console.log("clicked!!");
+    console.log("count is " + likeCounter);
+  }
+  return (
+    // JSX is this html thing,
+    // we can put JS inside this JSX
+    <div className="App">
+      <h1> Emoji Interpreter</h1>
+      <button onClick={likeClickHandler}>Like Me! </button> {likeCounter}
+    </div>
+  );
+}
+
+/**
+ *  things to notice
+ *  in react we use className instead of class
+ * class --> className
+ * beacause class is a reserved keyword in JS
+ * style --> JSX takes style as an object instead of string
+ */
