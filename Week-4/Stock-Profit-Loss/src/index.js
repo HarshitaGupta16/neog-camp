79b41ee620226b952fe3c3b924c27292391846d5
Harshita Gupta <47924686+HarshitaGupta16@users.noreply.github.com>
2021-09-07 18:16:53 +0530

    modify code

--- Week-4/Stock-Profit-Loss/src/index.js
+++ Week-4/Stock-Profit-Loss/src/index.js
@@ -1,61 +1,64 @@
 import "./styles.css";
 
-// document.getElementById("app").innerHTML = ``;
-
-const initialPrice = document.querySelector("#initial-price");
+const initialStockPrice = document.querySelector("#initial-price");
 const stocksQuantity = document.querySelector("#stocks-quantity");
-const currentPrice = document.querySelector("#current-price");
+const currentStockPrice = document.querySelector("#current-price");
 const btnRef = document.querySelector("#submit-btn");
-const outputBox = document.querySelector("#output-box");
-
-// console.log(initialPrice);
+const outputResult = document.querySelector("#output-result");
 
 // if (initialPrice === 0) {
 //   btnRef.disabled = true;
 // }
-btnRef.addEventListener("click", submitHandler);
 
-function submitHandler() {
-  let ip = initialPrice.value;
+if (initialStockPrice === 0) {
+  btnRef.disabled = true;
+}
+btnRef.addEventListener("submit", submitHandler);
+
+function submitHandler(event) {
+  event.preventDefault();
+  let iniPrice = initialStockPrice.value;
   let qty = stocksQuantity.value;
-  let curr = currentPrice.value;
+  let currPrice = currentStockPrice.value;
   // console.log(initialPrice)
-
-  calculateProitAndLoss(Number(ip), Number(qty), Number(curr));
+  calculateProitAndLoss(Number(iniPrice), Number(qty), Number(currPrice));
 }
 
-function calculateProitAndLoss(initialP, quantity, currentP) {
+function calculateProitAndLoss(initialPrice, quantity, currentPrice) {
   // if () {
   //   alertBox.innerHTML = "!! All fields are mandatory !!";
   // }
-
-  if (initialP > currentP) {
-    // Loss
-    let lossOnTotalQuantiy = (initialP - currentP) * quantity;
-    let lossPercent = ((lossOnTotalQuantiy / initialP) * 100).toFixed(2);
-    showOutput(
-      `The loss is ${lossOnTotalQuantiy} and loss percentage is ${lossPercent}`
-    );
-    outputBox.style.display = "block";
-    outputBox.style.backgroundColor = "#DC2626";
-  } else if (initialP < currentP) {
-    // Profit
-    let profitOnTotalQuantity = (currentP - initialP) * quantity;
-    let profitPercent = ((profitOnTotalQuantity / initialP) * 100).toFixed(2);
-    showOutput(`The profit is ${profitOnTotalQuantity} and the profit 
+  if (initialPrice !== 0 && quantity !== 0 && currentPrice !== 0) {
+    if (initialPrice > currentPrice) {
+      // Loss
+      let lossOnTotalQuantiy = (initialPrice - currentPrice) * quantity;
+      let lossPercent = ((lossOnTotalQuantiy / initialPrice) * 100).toFixed(2);
+      outputMessageDisplay(
+        `The loss is ${lossOnTotalQuantiy} and loss percentage is ${lossPercent}`
+      );
+      outputResult.style.display = "block";
+      outputResult.style.backgroundColor = "#DC2626";
+    } else if (initialPrice < currentPrice) {
+      // Profit
+      let profitOnTotalQuantity = (currentPrice - initialPrice) * quantity;
+      let profitPercent = (
+        (profitOnTotalQuantity / initialPrice) *
+        100
+      ).toFixed(2);
+      outputMessageDisplay(`The profit is ${profitOnTotalQuantity} and the profit 
     percentage is ${profitPercent} `);
-    outputBox.style.display = "block";
-    outputBox.style.backgroundColor = "#65A30D";
-  } else {
-    // no profit no loss
-    if (initialP !== 0 || quantity !== 0 || currentP !== 0) {
-      showOutput("No profit no loss!!");
-      outputBox.style.display = "block";
-      outputBox.style.backgroundColor = "gray";
+      outputResult.style.display = "block";
+      outputResult.style.backgroundColor = "#65A30D";
+    } else {
+      // no profit no loss
+
+      outputMessageDisplay("No profit no loss!!");
+      outputResult.style.display = "block";
+      outputResult.style.backgroundColor = "gray";
     }
   }
 }
 
-function showOutput(message) {
-  outputBox.innerHTML = message;
+function outputMessageDisplay(msg) {
+  outputResult.innerHTML = msg;
 }
